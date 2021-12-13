aadf40e891ad3c601be6b25e0f0ab7dfb66aee84
marievaba <marie.vaba@gmail.com>
2021-12-13 20:41:47 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ function weatherDataFetch(city) {
 function cityWeather(e) {
     weatherDataFetch('Tallinn');
 }
+
+function drawWeather(data) {
+    var celsius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celsius + '&deg;';
+    document.querySelector('#location').innerHTML = data.name;
+}
